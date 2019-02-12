9a1fecfe3e47d5ee204a082dacf1609f0d762db2
ManonFenery <46109740+ManonFenery@users.noreply.github.com>
2019-02-12 08:25:57 +0100

    Comment for def mailer attendance

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,11 +1,11 @@
 class Attendance < ApplicationRecord
 
-  after_create :new_participant
+  #after_create :new_participant
 
   belongs_to :guest, class_name: "User"
   belongs_to :event
 
-  def new_participant
-    AttendanceMailer.new_participant_email(self).deliver_now
-  end
+  #def new_participant
+    #AttendanceMailer.new_participant_email(self).deliver_now
+  #end
 end
